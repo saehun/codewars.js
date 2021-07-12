5cc92cdb9287462b96ec3c249502804ed09b2dd4
minidonut <nycom13@gmail.com>
2021-07-13 05:27:23 +0900

    feat(fetcher): Impl applicative functor

--- src/core/Fetcher.ts
+++ src/core/Fetcher.ts
@@ -1,5 +1,11 @@
 import { AxiosInstance } from 'axios';
 
+type AsyncReturnType<T extends (...args: any) => any> = T extends (...args: any) => Promise<infer U>
+  ? U
+  : T extends (...args: any) => infer U
+  ? U
+  : any;
+
 class Fetcher<T> {
   private constructor(private readonly fetch: (client: AxiosInstance) => Promise<T>) {}
   static of<T>(fetch: (client: AxiosInstance) => Promise<T>) {
@@ -17,6 +23,18 @@ class Fetcher<T> {
       await consume(out);
       return out;
     });
+  }
+  ap<I, O = T extends (arg: I) => any ? AsyncReturnType<T> : never>(
+    input: Fetcher<I>
+  ): O extends never ? never : Fetcher<O> {
+    return new Fetcher(async client => {
+      const fn = await this.run(client);
+      if (typeof fn === 'function') {
+        const arg = await input.run(client);
+        return fn(arg);
+      }
+      throw new TypeError(`${fn} is not a function`);
+    }) as O extends never ? never : Fetcher<O>;
   }
   map<O>(fn: (input: T) => Promise<O> | O): Fetcher<O> {
     return new Fetcher(async client => {
