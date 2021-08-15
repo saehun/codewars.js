0b94ce171d50d2ad48cef4ec3cc1f08799c41685
minidonut <nycom13@gmail.com>
2021-08-15 21:46:16 +0900

    feat: add language type and validator

---
+++ src/utils/validate-language.ts
@@ -0,0 +1,56 @@
+import { Language } from '../types';
+
+export function validateLanguage(given: string): asserts given is Language {
+  if (LANGUAGES.includes(given)) {
+    return;
+  }
+  throw new Error(`Not supported language: ${given}`);
+}
+
+const LANGUAGES = [
+  'crystal',
+  'csharp',
+  'elixir',
+  'elm',
+  'haskell',
+  'java',
+  'javascript',
+  'julia',
+  'prolog',
+  'python',
+  'ruby',
+  'swift',
+  'typescript',
+  'c',
+  'coffeescript',
+  'cpp',
+  'go',
+  'groovy',
+  'php',
+  'rust',
+  'dart',
+  'kotlin',
+  'nasm',
+  'pascal',
+  'r',
+  'scala',
+  'clojure',
+  'factor',
+  'fsharp',
+  'lua',
+  'nim',
+  'powershell',
+  'racket',
+  'raku',
+  'shell',
+  'objc',
+  'purescript',
+  'reason',
+  'sql',
+  'cfml',
+  'fortran',
+  'ocaml',
+  'perl',
+  'commonlisp',
+  'forth',
+];
